240d13f1105ad85dc2456f97f5967109a641e600
Biswa <razabpn@gmail.com>
2023-05-18 18:53:08 +0530

    TF v1 file for creating PubSub

---
+++ Main.tf
@@ -0,0 +1,21 @@
+
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">=4.51.0"
+    }
+  }
+}
+provider "google" {
+  credentials = file("login.json")
+}
+resource "google_pubsub_topic" "my_topic" {
+  name = "my_topic"
+}
+
+resource "google_pubsub_subscription" "my_subscription" {
+  name  = "my_sub"
+  topic = google_pubsub_topic.my_topic.name
+}
